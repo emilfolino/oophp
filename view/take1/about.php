4ded16e1cad4828e8a86b6a32b54939cd954be6e
Emil Folino BTH MAC <emilfolino@gmail.com>
2017-12-14 11:50:59 +0100

    Added photo and texts

--- view/take1/about.php
+++ view/take1/about.php
@@ -1,2 +1,2 @@
 <h1 class="title">About</h1>
-<p>This is the page about me.</p>
+<p>This is the <a href="https://dbwebb.se/kurser/oophp-v3">oophp</a> course at <a href="https://dbwebb.se/">dbwebb</a> which is the coolest education you can get. We give at <a href="https://bth.se/">BTH</a>.</p>
--- view/take1/home.php
+++ view/take1/home.php
@@ -1,2 +1,9 @@
-<h1 class="title">Home</h1>
-<p>This is the homepage.</p>
+<?php
+$imgUrl = $app->url->asset("image/me.jpg?w=300");
+
+?><h1 class="title">Home</h1>
+<p>This is my homepage for the oophp course.</p>
+
+<p>I am Emil Folino (efo) and I teach in the course. Besides teaching I do a bit of pedagogical research. And when I'm not at work I enjoy running real fast in the forrest.</p>
+
+<img src="<?= $imgUrl ?>" alt="Image of Emil Folino" />
